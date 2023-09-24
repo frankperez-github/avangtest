9b394aed1129e93d8da3e9ef546e1ad78b9ddabd
frankperez-github <fp848584@gmail.com>
2023-09-24 08:45:53 -0400

    first commit

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -6,8 +6,8 @@ export default function Main()
         <div className="main">
             <img src="/elipse.svg" alt="" className="elipse"/>
             <img src="/logo.svg" alt="logo" />
-            <h1 translate="yes">¡Pronto llegará Caribbean Payways, la nueva pasarela de pago para la región del Caribe!</h1>
-            <p translate="yes">¿Estás buscando una forma rápida, segura y conveniente de realizar tus
+            <h1>¡Pronto llegará Caribbean Payways, la nueva pasarela de pago para la región del Caribe!</h1>
+            <p>¿Estás buscando una forma rápida, segura y conveniente de realizar tus
                 transacciones en línea? ¡Caribbean Payways está aquí para hacerte la vida más
                 fácil! Con tecnología de vanguardia y una interfaz intuitiva, Caribbean Payways se
                 convertirá en tu perfecta herramienta para todas tus necesidades de pago
