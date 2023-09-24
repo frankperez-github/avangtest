8812934100c069b0b46d7f5edc24de2929aa8809
frankperez-github <fp848584@gmail.com>
2023-09-24 10:39:17 -0400

    tablet and mobile versions almost ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import './App.css';
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
 function App() {
-  const [joined, setJoined] = useState(false)
-    const handleSubmit = (e)=>{
-        e.preventDefault()
+    const [joined, setJoined] = useState(false)
+
+    const handleSubmit = ()=>{
         setJoined(true)
     }
     
     return(
         <div className="main" translate="yes">
-            <img src="/elipse.svg" alt="" className="elipse"/>
-            <div className="container">
+            <img src={window.innerWidth > 768 ? "/elipse.svg": window.innerWidth > 414 ? "/tabletElipse.svg" : "/mobileElipse.svg"} alt="" className="elipse"/>
+            <div className="container" style={{paddingTop: (joined && window.innerWidth <= 768)  && "20%"}}>
                 <img src="/logo.svg" alt="logo"  className="logo"/>
                 <h1>
                     {
-                        joined === false?
+                        !joined ?
                         "¡Pronto llegará Caribbean Payways, la nueva pasarela de pago para la región del Caribe!"
                         :
                         "Gracias por ser de los primeros en unirse"
@@ -24,16 +24,16 @@ function App() {
                 </h1>
                 <p>
                     {
-                        joined === false?
+                        !joined ?
                     "¿Estás buscando una forma rápida, segura y conveniente de realizar tus transacciones en línea? ¡Caribbean Payways está aquí para hacerte la vida más fácil! Con tecnología de vanguardia y una interfaz intuitiva, Caribbean Payways se convertirá en tu perfecta herramienta para todas tus necesidades de pago en línea."
                         :
                         "Cuando lancemos Caribbean Payways le enviaremos un correo de notificación. ¡Estamos en contacto!"
                     }
                 </p>
                 {   
-                    joined === false && 
-                    <form action="">
-                        <input type="email" name="" id="" placeholder="Introduzca su dirección de correo"/>
+                    !joined && 
+                    <form action="submit">
+                        <input required="true" type="email" name="" id="" placeholder="Introduzca su dirección de correo"/>
                         <button className="siteButton" onClick={handleSubmit}>
                             <div className="buttonContent">
                                 <img src="/check.svg" alt="" />
