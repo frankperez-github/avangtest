c61bac6d77c4766a659e9e4f66c8196f92a4620b
frankperez-github <fp848584@gmail.com>
2023-09-25 19:17:41 -0400

    css mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { useEffect, useState } from "react"
+import {  useState } from "react"
 
 
 function App() {
@@ -12,37 +12,39 @@ function App() {
     
     return(
         <div className="main" translate="yes">
-            <img src={window.innerWidth > 768 ? "/elipse.svg": window.innerWidth > 425 ? "/tabletElipse.svg" : "/mobileElipse.svg"} alt="" className="elipse"/>
-            <div className="container" style={{paddingTop: (joined && window.innerWidth <= 768)  && "20%"}}>
-                <img src="/logo.svg" alt="logo"  className="logo"/>
-                <h1>
-                    {
-                        !joined ?
-                        "¡Pronto llegará Caribbean Payways, la nueva pasarela de pago para la región del Caribe!"
-                        :
-                        "Gracias por ser de los primeros en unirse"
+            {/* <img src={window.innerWidth > 768 ? "/elipse.svg": window.innerWidth > 425 ? "/tabletElipse.svg" : "/mobileElipse.svg"} alt="" className="elipse"/> */}
+            <div className="circle">
+                <div className="container" style={{paddingTop: (joined && window.innerWidth <= 768)  && "20%"}}>
+                    <img src="/logo.svg" alt="logo"  className="logo"/>
+                    <h1>
+                        {
+                            !joined ?
+                            "¡Pronto llegará Caribbean Payways, la nueva pasarela de pago para la región del Caribe!"
+                            :
+                            "Gracias por ser de los primeros en unirse"
+                        }
+                    </h1>
+                    <p>
+                        {
+                            !joined ?
+                        "¿Estás buscando una forma rápida, segura y conveniente de realizar tus transacciones en línea? ¡Caribbean Payways está aquí para hacerte la vida más fácil! Con tecnología de vanguardia y una interfaz intuitiva, Caribbean Payways se convertirá en tu perfecta herramienta para todas tus necesidades de pago en línea."
+                            :
+                            "Cuando lancemos Caribbean Payways le enviaremos un correo de notificación. ¡Estamos en contacto!"
+                        }
+                    </p>
+                    {   
+                        !joined && 
+                        <form action="submit" onSubmit={handleSubmit}>
+                            <input required="true" type="email" name="" id="" placeholder="Introduzca su dirección de correo"/>
+                            <button className="siteButton">
+                                <div className="buttonContent">
+                                    <img src="/check.svg" alt="" />
+                                    <p>Unirme a la lista de espera</p>
+                                </div>
+                            </button>
+                        </form>
                     }
-                </h1>
-                <p>
-                    {
-                        !joined ?
-                    "¿Estás buscando una forma rápida, segura y conveniente de realizar tus transacciones en línea? ¡Caribbean Payways está aquí para hacerte la vida más fácil! Con tecnología de vanguardia y una interfaz intuitiva, Caribbean Payways se convertirá en tu perfecta herramienta para todas tus necesidades de pago en línea."
-                        :
-                        "Cuando lancemos Caribbean Payways le enviaremos un correo de notificación. ¡Estamos en contacto!"
-                    }
-                </p>
-                {   
-                    !joined && 
-                    <form action="submit" onSubmit={handleSubmit}>
-                        <input required="true" type="email" name="" id="" placeholder="Introduzca su dirección de correo"/>
-                        <button className="siteButton">
-                            <div className="buttonContent">
-                                <img src="/check.svg" alt="" />
-                                <p>Unirme a la lista de espera</p>
-                            </div>
-                        </button>
-                    </form>
-                }
+                </div>
             </div>
             <img src="/creditcards.svg" alt="" className="cCards"/>
         </div>
